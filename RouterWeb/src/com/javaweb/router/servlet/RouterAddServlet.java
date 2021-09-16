99590b7ff2f637cfe150a12a2d3f19ac631af343
kuukie <3260681415@qq.com>
2021-09-16 09:26:14 +0800

    11

---
+++ RouterWeb/src/com/javaweb/router/servlet/RouterAddServlet.java
@@ -0,0 +1,28 @@
+package com.javaweb.router.servlet;
+
+import com.javaweb.router.bean.HostMessage;
+import com.javaweb.router.util.RouterWebUtils;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import net.sf.json.JSONArray;
+
+import java.io.IOException;
+
+public class RouterAddServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HostMessage hostMessage = RouterWebUtils.hostService.getMessage();
+
+        JSONArray jsonArray = JSONArray.fromObject(hostMessage);
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().println(jsonArray);
+        System.out.println(jsonArray);
+    }
+}
